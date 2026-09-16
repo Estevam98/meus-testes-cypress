3a85a2f65a767dddfe557cad3f8db98678479a02
Lucas Estevam <lucas_estevam98@hotmail.com>
2026-09-16 17:17:04 -0300

    feat: add beforeEach hook to Select Menu test

--- cypress/e2e/demoqa-selectmenu.cy.ts
+++ cypress/e2e/demoqa-selectmenu.cy.ts
@@ -1,8 +1,11 @@
 import SelectMenuPage from '../pages/SelectMenuPage'
 
 describe('DemoQA - Select Menu', () => {
-  it('deve selecionar uma opção no dropdown', () => {
+  beforeEach(() => {
     SelectMenuPage.visit()
+  })
+
+  it('deve selecionar uma opção no dropdown', () => {
     SelectMenuPage.buttonSelectValue()
     SelectMenuPage.selectOption()
     SelectMenuPage.checkMessage()
