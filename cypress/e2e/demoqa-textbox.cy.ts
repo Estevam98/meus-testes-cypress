9213d5436a955904f494a064f1ca21e5532b866a
Lucas Estevam <lucas_estevam98@hotmail.com>
2026-09-18 17:11:37 -0300

    refactor: separate describes and add beforeEach for TextBox tests

--- cypress/e2e/demoqa-textbox.cy.ts
+++ cypress/e2e/demoqa-textbox.cy.ts
@@ -3,16 +3,28 @@ import ButtonsPage from '../pages/ButtonsPage'
 import CheckboxPage from '../pages/CheckboxPage'
 import RadioButtonPage from '../pages/RadioButtonPage'
 
-describe('DemoQA - Testes E2E', () => {
+describe('DemoQA - Text Box', () => {
+  beforeEach(() => {
+    TextBoxPage.visit()
+  })
 
   it('deve preencher o formulário com Page Object', () => {
-    TextBoxPage.visit()
     TextBoxPage.fillName('Lucas Estevam')
     TextBoxPage.fillEmail('lucas@teste.com')
     TextBoxPage.submit()
     cy.get('#name').should('contain', 'Lucas Estevam')
   })
 
+  it('Formulário sem o Email', () => {
+    cy.get('#userName').type('Lucas Estevam')
+    cy.get('#currentAddress').type('Isso é um teste')
+    cy.get('#permanentAddress').type('Isso realmente é um teste')
+    cy.get('#submit').click()
+    cy.get('.border').should('not.include.text', 'Email')
+  })
+})
+
+describe('DemoQA - Outros testes', () => {
   it('deve clicar no botão e ver a mensagem', () => {
     ButtonsPage.visit()
     ButtonsPage.buttonClickMe()
@@ -29,15 +41,5 @@ describe('DemoQA - Testes E2E', () => {
     RadioButtonPage.visit()
     RadioButtonPage.buttonYes()
     RadioButtonPage.checkMessage()
-  })
-
-  it('Formulário sem o Email', () => {
-    cy.visit('https://demoqa.com/text-box')
-    cy.get('#userName').type('Lucas Estevam') 
-    cy.get('#currentAddress').type('Isso é um teste')
-    cy.get('#permanentAddress').type('Isso realmente é um teste') 
-    cy.get('#submit').click() 
-    cy.get('.border').should('not.include.text', 'Email')
-
   })
 })
