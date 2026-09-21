4de71961c577727281398123efdff381cc933002
Lucas Estevam <lucas_estevam98@hotmail.com>
2026-09-21 18:20:23 -0300

    feat: add complete signup and login flow tests

--- cypress/e2e/AutomationExercise/login.cy.ts
+++ cypress/e2e/AutomationExercise/login.cy.ts
@@ -5,8 +5,33 @@ describe('Automation Exercise - Criação de Conta', () => {
 
   it('deve preencher o formulário de cadastro e avançar', () => {
     cy.get('[data-qa="signup-name"]').type('Lucas Estevam')
-    cy.get('[data-qa="signup-email"]').type('testeqa2@qateste.com')
+    cy.get('[data-qa="signup-email"]').type('teste123@teste.com')
     cy.get('[data-qa="signup-button"]').click()
     cy.url().should('include', '/signup')
+    cy.get('[value="Mr"]').click()
+    cy.get('[data-qa="password"]').type('Senha@123')
+    cy.get('[data-qa="first_name"]').type('Lucas')
+    cy.get('[data-qa="last_name"]').type('Estevam')
+    cy.get('[data-qa="address"]').type('Rua Teste, 123')
+    cy.get('[data-qa="country"]').select('United States')
+    cy.get('[data-qa="state"]').type('California')
+    cy.get('[data-qa="city"]').type('Los Angeles')
+    cy.get('[data-qa="zipcode"]').type('12345')
+    cy.get('[data-qa="mobile_number"]').type('1234567890')
+    cy.get('[data-qa="create-account"]').click()
+    cy.url().should('include', '/account_created')
+  })
+})
+
+describe('Automation Exercise - Login', () => {
+  beforeEach(() => {
+    cy.visit('https://automationexercise.com/login')
+  })
+
+  it('deve logar com sucesso', () => {
+    cy.get("[data-qa='login-email']").type('teste123@teste.com')
+    cy.get("[data-qa='login-password']").type('Senha@123')
+    cy.get('[data-qa="login-button"]').click()
+    cy.get('a').contains('Logout').should('be.visible')
   })
 })
